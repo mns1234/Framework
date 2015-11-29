1b4c1b917196353c6a90131ba4293ef999de5dda
mns1234 <mns1234@yahoo.com>
2015-11-28 18:54:53 -0800

    hi

---
+++ WebDriverDemo1/src/com/test/GmailRegistrationTest.java
@@ -0,0 +1,21 @@
+package com.test;
+
+import static org.testng.AssertJUnit.assertEquals;
+import org.testng.annotations.Test;
+
+import com.main.GmailRegistration;
+
+public class GmailRegistrationTest {
+	
+	@Test
+	public void testValidateform(){
+	GmailRegistration gr = new 	GmailRegistration();
+	String actual=gr.validateform();
+	String expected="You can't leave this empty.";
+	assertEquals(expected,actual);
+	}
+
+	
+	}
+
+
